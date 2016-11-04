0c1e0bc4fb39c3b58361952425277f9dadbdbedc
Ildar <ildaryxa@gmail.com>
2016-11-04 21:20:46 +0500

    виды форм

--- src/java/enumType/FormType.java
+++ src/java/enumType/FormType.java
@@ -7,5 +7,11 @@ public enum FormType {
         RegistrationAdmin,  //регистрация из админки
         UpdateAdmin, //изменение из админки
         Update, //изменить
-        Registration // регистрация
+        Registration, // регистрация
+        addEmployee, //добавление нового сотрудника
+        changeEmployee, //изменение данных сотрудника
+        addContract,  //добавление контракта
+        changeContract, //изменение контракта
+        userView, //пользовательский просмотр
+        adminView //просмотр из админки
 }
